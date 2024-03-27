80047201f64911153d37325d23f8187399e337fc
kumarmishraayush <kumarmishraayush@gmail.com>
2024-03-27 10:34:35 +0530

    Made Point Card

--- src/Components/AppContainer/Left/Left.jsx
+++ src/Components/AppContainer/Left/Left.jsx
@@ -1,5 +1,5 @@
 import React from "react";
- 
+
 import "./Left.css";
 const Left = () => {
   return (
@@ -16,8 +16,7 @@ const Left = () => {
 
           <div className="basic-info">
             <div className="basic-info1">
-               
-             <h6 >500</h6>
+              <h6>500</h6>
               <h5 className="countFollow">Followers</h5>
             </div>
 
@@ -27,14 +26,31 @@ const Left = () => {
             </div>
 
             <div className="basic-info3">
-            
-              <h6 >500</h6>
-              <h6 className="countFollow">Followning</h6>
+              <h6>500</h6>
+              <h5 className="countFollow">Followning</h5>
             </div>
-            
-            
           </div>
-              <button id="basic-info-btn">View Profile</button>
+          <button id="basic-info-btn">View Profile</button>
+        </div>
+
+        <div className="left-PointContainer">
+          <div className="left-heading-points">
+            <div className="left-point-analysis">
+              <ul>
+                <li>Let's Analyse the Person</li>
+              </ul>
+            </div>
+          </div>
+          <div className="person-points">
+            <div className="good-points">
+              <img src="good-person.png" alt="Good-Person" />
+              <h5>Good Points:550</h5>
+            </div>
+            <div className="bad-points">
+              <img src="evil-person.png" alt="Evil-Person" />
+              <h5>Bad Points:500</h5>
+            </div>
+          </div>
         </div>
         {/* Ethics Rank In Friend */}
         <div className="left-rank"></div>
