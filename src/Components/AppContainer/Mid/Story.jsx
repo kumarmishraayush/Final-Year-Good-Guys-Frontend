1ea83fc95a8fce42c67ebb99fb5434d538c835b0
kumarmishraayush <kumarmishraayush@gmail.com>
2024-03-28 09:52:25 +0530

    adding story section

--- src/Components/AppContainer/Mid/Story.jsx
+++ src/Components/AppContainer/Mid/Story.jsx
@@ -1,53 +1,71 @@
-import React from 'react'
-
+import React from "react";
+import "./Story.css"
 const Story = () => {
-  const data = [{
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  },
-  {
-    "storyOwner":"ProfilePic.jpg",
-    "storyPost":"Mera.jpg"
-  }]
-
+  const data = [
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+    {
+      storyOwner: "ProfilePic.jpg",
+      storyPost: "Mera.jpg",
+    },
+  ];
 
   return (
-    <div className='story-main'>
-      
+    <div className="story-main">
+        {data.map((item, index) => (
+          <div key={index}>
+            {" "}
+            {/* Adding a unique key for each item */}
+            <div className="story-card">
+            <img
+              className="story-ownerIMG"
+              src={item.storyOwner}
+              alt="story owner"
+            />
+            <img
+              className="story-postIMG"
+              src={item.storyPost}
+              alt="story-post"
+            />
+      </div>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Story
+export default Story;
