5a16c19c1de9cb32d36d19b6e2ddc396af54b984
kumarmishraayush <kumarmishraayush@gmail.com>
2024-04-15 19:15:22 +0530

    adding comment box

--- src/Components/AppContainer/Mid/Post.jsx
+++ src/Components/AppContainer/Mid/Post.jsx
@@ -5,6 +5,8 @@ import { SlOptionsVertical } from "react-icons/sl";
 import { BiSolidLike } from "react-icons/bi";
 import { BiSolidDislike } from "react-icons/bi";
 import { LiaCommentDotsSolid } from "react-icons/lia";
+import { CiFaceSmile } from "react-icons/ci";
+
 
 const Post = () => {
   const PostData = [
@@ -87,12 +89,19 @@ I'm not trying to be everyone's cup of tea. I'm my own double-shot espresso
 I'm not chasing anyone; I'm too busy chasing my dreams{PostData[0].Name}
 My attitude is the weapon that takes me places</div>
       </div>
-      <hr style={{margin:"auto", width:"80%", marginTop:"10px",   backgroundColor:"var(--color-text-secondary),borderTop: '1px solid #000'"}}/>
+      
       <div className="post-lower">
         <div className="post-lower-like"><BiSolidLike size={30}  />25</div>
         <div className="post-lower-comment"><LiaCommentDotsSolid  size={30}  />25</div>
         <div className="post-lower-dislike"><BiSolidDislike size={30}  />25</div>
       </div>
+      <hr style={{margin:"auto", width:"80%", marginTop:"10px",   backgroundColor:"var(--color-text-secondary),borderTop: '1px solid #000'"}}/>
+      <div className="post-addComment">
+        <img src="ProfilePic.jpg" alt="ProfilePic" />
+        <input type="text" placeholder="Add a comment" />
+        <CiFaceSmile size={25} className="comment-smile"/>
+        </div>
+    
     </div>
     </div>
   );
