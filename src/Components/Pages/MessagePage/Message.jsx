45e8e690d6bd70bc470b59f30dc6fa1a42ee4e08
kumarmishraayush <kumarmishraayush@gmail.com>
2024-04-19 13:53:00 +0530

    hi sir adding message

--- src/Components/Pages/MessagePage/Message.jsx
+++ src/Components/Pages/MessagePage/Message.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import Followers from '../../Freinds/Followers/Followers';
+import Followings from '../../Freinds/Followings/Followings';
 
 function Message() {
   return (
@@ -9,7 +11,8 @@ function Message() {
     animate={{ x: 0 }} // Move to x=0 (center of the viewport)
     transition={{ duration: .5 }} // Animation duration
     style={{ width: "100%", height: "100%" }}>
-      Message
+      <Followers/>
+      <Followings/>
     </motion.div>
    </>
   )
