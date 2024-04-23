093461e60e33369e4ed15253e802f50c1db53d5d
kumarmishraayush <kumarmishraayush@gmail.com>
2024-04-23 08:23:32 +0530

    some changes in login form

--- src/Components/Pages/LoginPage/Login.jsx
+++ src/Components/Pages/LoginPage/Login.jsx
@@ -1,41 +1,71 @@
-import React from 'react'
-import { motion } from "framer-motion";
-import "./Login.css"
- 
-import { CiLock } from "react-icons/ci";
-function Login() {
-  return (
-    
-   <>
-   <motion.div 
-   initial={{ x: -1000 }} // Initial position outside the viewport
-   animate={{ x: 0 }} // Move to x=0 (center of the viewport)
-   transition={{ duration: .5 }} // Animation duration
-   style={{ width: "100%", height: "100%" }}>
-     <div className='login-main'>
-      <div className='lock'>
-      <CiLock size={50}  />
-      </div>
-      <div className ='login-header'>Login</div>
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { CiLock } from 'react-icons/ci';
+import './Login.css';
 
-      <div className='login-body'>
-          Enter Your Email
-        <div className='login-email'>
-        <input type="text"  name ="email" placeholder='Enter Email' />
+function Login() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form data:', formData);
+    // You can add further logic like sending the data to an API here
+  };
+
+  return (
+    <motion.div
+      initial={{ x: -1000 }} // Initial position outside the viewport
+      animate={{ x: 0 }} // Move to x=0 (center of the viewport)
+      transition={{ duration: 0.5 }} // Animation duration
+      className="login-main-container"
+    >
+      <div className="login-main">
+        <div className="lock">
+          <CiLock size={50} />
         </div>
-        Enter Your Password
-        <div className='login-password'>
-        <input type="password" name="password" placeholder='Enter Password' />
-        </div>
-        <button type="submit">Login</button>
-        <span>Don't have an account?</span>
-        <a href="/signup">Signup</a>
+        <div className="login-header">Login</div>
+
+        <div className="login-body">
+          <form onSubmit={handleSubmit}>
+            Enter Your Email
+            <div className="login-email">
+              <input
+                type="text"
+                name="email"
+                placeholder="Enter Email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            Enter Your Password
+            <div className="login-password">
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter Password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+            <motion.button className='login-submit'
+              type="submit"
+              whileHover={{ scale: 1.1 }} // Scale animation on hover
+              whileTap={{ scale: 0.9 }} // Scale animation on tap/click
+            >
+              Login
+            </motion.button>
+          </form>
+          <span>Don't have an account?</span>
+          <a href="/signup">Signup</a>
         </div>
       </div>
-   </motion.div>
-   
-  </>
-  )
+    </motion.div>
+  );
 }
 
-export default Login
+export default Login;
